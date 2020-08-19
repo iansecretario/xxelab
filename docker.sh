b60740162dc682a153bbf5f2004645715b29c9d9
ian <m.christiansecretario@gmail.com>
2020-08-20 03:02:32 +0800

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt install docker-ce -y
+systemctl start docker
