dc47373b926b131ab371f20421c278b0c5c0094f
ian <m.christiansecretario@gmail.com>
2020-08-22 05:06:58 +0800

    Update registration.php

--- penlab/registration.php
+++ penlab/registration.php
@@ -1,9 +1,9 @@
 <?php
 $file = $_GET['file'];
 
-/**
-* The registration page includes confidential data that can be abused by adversaries
-*  This is a sample source code date that can be exfiltrated out of the target server
-*/
+#This code is vulnerable to local file inclusion
+#The registration page includes confidential data that can be abused by adversaries
+#This is a sample source code date that can be exfiltrated out of the target server
+
 include('directory/' . $file);
 ?>
